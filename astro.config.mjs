99a915f1c0e3fc3fb9a6ac3b092ce46689a51238
上田尚道 <>
2024-11-14 15:53:21 +0900

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	output: "hybrid",//hybridにすることで、静的生成するページとSSRするページを設定できるようにします
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  //hybridにすることで、静的生成するページとSSRするページを設定できるようにします
+  output: "hybrid",
+  adapter: vercel()
 });
